8d9b2073ee57d7c45492e5d6565af836b37ac3aa
Henrik Lissner <henrik@lissner.net>
2019-01-01 16:08:11 -0500

    base/arch-desktop: simplify install-aseprite.sh

--- base/arch-desktop/scripts/install-aseprite.sh
+++ base/arch-desktop/scripts/install-aseprite.sh
@@ -5,12 +5,15 @@ if command -v aseprite >/dev/null; then
   exit 1
 fi
 
-set -e
-yay --needed --noconfirm -S ninja cmake libxcursor
-rm -rf /tmp/aseprite
-if git clone --recursive https://github.com/aseprite/aseprite /tmp/aseprite; then
-  mkdir /tmp/aseprite/build && cd /tmp/aseprite/build
-  cmake -DCMAKE_INSTALL_PREFIX=/usr -G Ninja ..
-  ninja aseprite
-  ninja install
-fi
+# Install dark theme from: https://github.com/tungkradle/aseprite-than/releases
+yay -S --needed aseprite
+
+# set -e
+# yay --needed --noconfirm -S ninja cmake libxcursor
+# rm -rf /tmp/aseprite
+# if git clone --recursive https://github.com/aseprite/aseprite /tmp/aseprite; then
+#   mkdir /tmp/aseprite/build && cd /tmp/aseprite/build
+#   cmake -DCMAKE_INSTALL_PREFIX=/usr -G Ninja ..
+#   ninja aseprite
+#   ninja install
+# fi
